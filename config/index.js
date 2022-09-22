fd891ce35b32e2d62b224dc7975f4eda4678ac25
Nikoyaya <58239518+Nikoyaya@users.noreply.github.com>
2022-09-23 02:33:20 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -60,15 +60,15 @@ export const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "ogNcm6IODx9QAA89uJ9oZM2N0LaE",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "J_ywKKWzFyLKBImhwGXSE5n4xq4IGZckAHAHl5aWaQU",
       // 所在省份
       province: "广东",
       // 所在城市
-      city: "肇庆",
+      city: "珠海",
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '12-27',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
